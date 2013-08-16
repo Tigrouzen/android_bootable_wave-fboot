e8b5021049a2fb44f3195a9e52f4747559116193
Tigrouzen <vartanjean7@gmail.com>
2013-08-15 21:37:01 -0700

    Memory Card & Modem Init

--- src/BL3_FBOOT_S8500_b2x.c
+++ src/BL3_FBOOT_S8500_b2x.c
@@ -6,12 +6,12 @@ unsigned long c_disp_FOTA_Printf[] = { 0xb69c410b, 0 };
 unsigned long c_OemSysGetSystemInfo[] = { 0xc3ac31a5, 0 };
 unsigned long c___PfsNandInit[] = { 0xa5fef2be, 0 };
 unsigned long c___PfsMassInit[] = { 0x24428179, 0 };
-//unsigned long c_MemoryCardMount[] = { 0x?, 0 };
+unsigned long c_MemoryCardMount[] = { 0x1a8b3571, 0 };
 unsigned long c_tfs4_stat[] = { 0x603fe251, 0 };
 unsigned long c_tfs4_open[] = { 0x3494b08a, 0 };
 unsigned long c_tfs4_read[] = { 0xc6a58dfd, 0 };
 unsigned long c_tfs4_close[] = { 0x0f926882, 0 };
-
+unsigned long c_DRV_Modem_BootingStart[] = { 0x5557427c, 0 };
 
 
 unsigned long *fun_crc[i_endMarker] = {c_MemMMUCacheEnable,
@@ -20,7 +20,8 @@ unsigned long *fun_crc[i_endMarker] = {c_MemMMUCacheEnable,
                                        c_OemSysGetSystemInfo,
                                        c___PfsNandInit,
                                        c___PfsMassInit,
-//                                       c_MemoryCardMount,
+                                       c_MemoryCardMount,
+	c_DRV_Modem_BootingStart,
                                        c_tfs4_stat,
                                        c_tfs4_open,
                                        c_tfs4_read,
